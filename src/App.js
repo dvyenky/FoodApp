3d68fa408bb2710da4131daf5095c55d738411e4
dvyenky <dvyenkatesh@gmail.com>
2025-11-24 23:44:34 +0530

    Create a Parcel and Tailwind Css Code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Fast Food Online Application 
+      </h1>
+
+      <p className="mt-4 text-lg">
+        Everything is working perfectly Lets Start Programing.
+      </p>
+    </div>
+  );
+}
